1e0721f84b8e81d3a7e385b24a0cf46de469801b
Chris Rrapi <toadawes12@gmail.com>
2018-12-15 12:48:24 -0500

    test stylechange

--- webscreener.py
+++ webscreener.py
@@ -1,5 +1,5 @@
 from quart import Quart
-from quart import request, jsonify
+from quart import request, jsonify, render_template
 import os
 
 import aiohttp
@@ -9,17 +9,23 @@ from arsenic.services import Chromedriver
 
 app = Quart(__name__)
 
+
 async def make_snapshot(website: str):
-    
+
     service = Chromedriver(log_file=os.devnull)
-    browser = Chrome(chromeOptions={ 'args': ['--headless', 
-                                              '--disable-gpu', 
-                                              '--hide-scrollbars', 
-                                              '--window-size=1920,1080', 
-                                              '--no-gpu',
-                                              '--ipc-connection-timeout=10',
-                                              '--disable-mojo-local-storage',
-                                             ] })
+    browser = Chrome(
+        chromeOptions={
+            "args": [
+                "--headless",
+                "--disable-gpu",
+                "--hide-scrollbars",
+                "--window-size=1920,1080",
+                "--no-gpu",
+                "--ipc-connection-timeout=10",
+                "--disable-mojo-local-storage",
+            ]
+        }
+    )
 
     async with get_session(service, browser) as session:
         await session.get(website)
@@ -28,44 +34,42 @@ async def make_snapshot(website: str):
 
         async with aiohttp.ClientSession() as sess:
 
-            headers = {
-                "Authorization": "Client-ID 6656d64547a5031"
-            }
-            data = {
-                "image": image,
-            }
+            headers = {"Authorization": "Client-ID 6656d64547a5031"}
+            data = {"image": image}
 
-            async with sess.post("https://api.imgur.com/3/image", data=data, headers=headers) as r:
+            async with sess.post(
+                "https://api.imgur.com/3/image", data=data, headers=headers
+            ) as r:
                 link = (await r.json())["data"]["link"]
 
                 return link
 
+
 @app.route("/")
 async def main():
-    return "OwO, what's this? Made by F4stZ4p#3507 and chr1s#7185 with ❤"
+    return render_template("info.html")
 
+@app.route("/api/v1", methods=["POST"])
 @app.route("/v1", methods=["POST"])
 async def web_screenshot():
-    
-    
+
     website = request.headers.get("website")
-    
+
     if website is None:
-        return jsonify({
-            "snapshot": "https://i.imgur.com/ZHPcdlW.jpg",
-            "website": "was not specified",
-            "status": 404
-            })
-    
+        return jsonify(
+            {
+                "snapshot": "https://cdn1.iconfinder.com/data/icons/web-interface-part-2/32/circle-question-mark-512.png",
+                "website": "A website was not provided.",
+                "status": 400,
+            }
+        )
+
     if not (website.startswith("http://") or website.startswith("https://")):
         website = f"http://{website}"
-    
+
     snapshot = await make_snapshot(website)
 
-    return jsonify({
-        "snapshot": snapshot,
-        "website": website,
-        "status": 200
-        })
+    return jsonify({"snapshot": snapshot, "website": website, "status": 200})
+
 
 app.run(host="0.0.0.0", port=os.getenv("PORT"), debug=True)

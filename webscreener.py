972ddcac0f11e535597a8526317493b1818847dc
Chris Rrapi <toadawes12@gmail.com>
2018-12-15 08:40:07 -0500

    move to quart and apply test fixes

--- webscreener.py
+++ webscreener.py
@@ -1,17 +1,13 @@
-from sanic import Sanic
-from sanic import response
-from signal import signal, SIGINT
-import asyncio
+from quart import Quart
+from quart import request, jsonify
 import os
-import random
 
 import aiohttp
 from arsenic import get_session
 from arsenic.browsers import Chrome
 from arsenic.services import Chromedriver
-from io import BytesIO
 
-app = Sanic(__name__)
+app = Quart(__name__)
 
 async def make_snapshot(website: str):
     
@@ -38,30 +34,19 @@ async def make_snapshot(website: str):
                 return link
 
 @app.route("/")
-async def main(request):
-    return response.text(
-        "OwO, what's this? Made by F4stZ4p#3507 with ❤"
-        )
+async def main():
+    return "OwO, what's this? Made by F4stZ4p#3507 and chr1s#7185 with ❤"
 
 @app.route("/v1/<website>")
-async def web_screenshot(request, website):
+async def web_screenshot(website):
+    if not website.startswith("http"):
+        snap = await make_snapshot(f"https://{website}")
+    else:
+        snap = await make_snapshot(website)
 
-    snap = await make_snapshot(f"https://{website}")
-
-    return response.json({
-        "status": response.status,
+    return jsonify({
         "snap": snap,
         "website": website
         })
 
-server = app.create_server(host="0.0.0.0", port=os.getenv("PORT"))
-
-loop = asyncio.get_event_loop()
-task = asyncio.ensure_future(server)
-asyncio.set_event_loop(asyncio.SelectorEventLoop())
-signal(SIGINT, lambda s, f: loop.stop())
-
-try:
-    loop.run_forever()
-except:
-    loop.stop()
+app.run(host="0.0.0.0", port=os.getenv("PORT"), debug=True)
